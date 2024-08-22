a375f11fe357e8338d16b978922f8d94a8f4e20c
yaseen.vws <yaseenahmad@vizzwebsolutions.com>
2024-08-22 09:36:43 +0500

    update

--- src/components/ShowComponent.jsx
+++ src/components/ShowComponent.jsx
@@ -29,6 +29,7 @@ function ShowComponent() {
   return (
     <div className='App' >
       <h1>Post</h1>
+      <h5><a className='btn btn-primary' href="/">Create New Post</a></h5>
      {posts.data != undefined && posts.data.data.length > 0 &&
      (
       <table style={{ width: '100%' }} border='1' >
@@ -48,7 +49,7 @@ function ShowComponent() {
               <td>{post.title}</td>
               <td>{post.date}</td>
               <td> <img src={'http://localhost:5000/api/postImages/'+post.image} alt="" style={{ width: '100px',height: '100px' }} /> </td>
-              <td><button  id={post._id} onClick={(e) => deletePost(post._id,e) } >Delete</button></td>
+              <td><button className='btn btn-danger'  id={post._id} onClick={(e) => deletePost(post._id,e) } >Delete</button></td>
               <td> <UpdateModelComponent id={post._id} title={post.title} date={post.date} /> </td>
             </tr>
 
